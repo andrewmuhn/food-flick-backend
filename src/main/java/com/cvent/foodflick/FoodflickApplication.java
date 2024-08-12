18ca53cbb7492a95a8472bccd7e9fa5682e80344
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-08-12 07:29:43 -0700

    init spring initializer

---
+++ src/main/java/com/cvent/foodflick/FoodflickApplication.java
@@ -0,0 +1,13 @@
+package com.cvent.foodflick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodflickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodflickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvent/foodflick/FoodflickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvent.foodflick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodflickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
