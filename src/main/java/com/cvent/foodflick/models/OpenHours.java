426455a095ccd1a8a686304ad0037f45e0b95cd0
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-08-14 14:20:19 -0700

    added Yelp Meta Data

---
+++ src/main/java/com/cvent/foodflick/models/OpenHours.java
@@ -0,0 +1,48 @@
+package com.cvent.foodflick.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class OpenHours {
+    private boolean is_overnight;
+    private int start;
+    @Column(name = "\"end\"")
+    private int end;
+    private int day;
+
+    public OpenHours() {
+    }
+
+    public boolean isIs_overnight() {
+        return is_overnight;
+    }
+
+    public void setIs_overnight(boolean is_overnight) {
+        this.is_overnight = is_overnight;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+}
