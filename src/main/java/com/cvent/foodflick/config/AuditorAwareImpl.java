f2ef1831c61bd49ef1e3e189e41b167788079b9b
lesleytomosada <tomosadalesley@gmail.com>
2024-08-15 14:50:10 -0700

    forgot an annotation, spring security all working

--- src/main/java/com/cvent/foodflick/config/AuditorAwareImpl.java
+++ src/main/java/com/cvent/foodflick/config/AuditorAwareImpl.java
@@ -3,9 +3,11 @@ package com.cvent.foodflick.config;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
